cd6e7b2232c424b5384dbea26d38894f69b83a21
Rodrigo G. Oliveira <mjgargani@gmail.com>
2022-01-31 16:09:11 -0300

    change: it descriptions and stub position

--- tests/createUsers.test.js
+++ tests/createUsers.test.js
@@ -5,6 +5,7 @@ const chaiHttp = require('chai-http');
 const server = require('../api/app');
 
 const { User } = require('../models');
+// Importação do mock utilizado nesse contexto
 const { User: userMock }  = require('./mock/models')
 
 chai.use(chaiHttp);
@@ -12,25 +13,57 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Rota /api/users', () => {
-    describe('Testa a inserção de registros através da rota', () => {
+    before(() => {
+        // Intercepta, com stubs, os métodos utilizados pelo modelo do
+        // sequelize e os substitui pelos nossos mocks, através da função
+        // `callsFake`, antes de começarmos os testes
+        sinon.stub(User, 'create')
+            .callsFake(userMock.create);
+        sinon.stub(User, 'findAll')
+            .callsFake(userMock.findAll);
+    });
+
+    after(() => {
+        // Restaura o comportamento normal dos métodos do modelo
+        // (Remove a interceptação), após os testes
+        User.create.restore();
+        User.findAll.restore();
+    })
+
+    describe('Consulta a lista de pessoas usuárias', () => {
+        let response;
+
+        before(async () => {         
+            response = await chai
+                .request(server)
+                .get('/api/users');
+        });
+
+        it('A requisição GET para a rota traz uma lista inicial contendo dois registros de pessoas usuárias', () => {
+            expect(response.body).to.have.length(2);
+        });
+
+        it('Essa requisição deve retornar código de status 200', () => {
+            expect(response).to.have.status(200);
+        });
+    });
+
+    describe('Insere um novo registro', () => {
         let createRequest = {};
         let firstUserList = [];
         let secondUserList = [];
+        const newUser = {
+            username: 'jane',
+            password: 'senha123'
+        };
 
         before(async () => {
-            // Intercepta os métodos utilizados pelo modelo do sequelize
-            // e os substitui pelos nossos
-            sinon.stub(User, 'create')
-                .callsFake(userMock.create);
-            sinon.stub(User, 'findAll')
-                .callsFake(userMock.findAll);
-            
             /*
-                São feitas três ações:
-                (1) Validação da lista de usuários inicial
-                (2) Criação de um novo usuário
+                Antes de validar os testes, serão feitas três requisições:
+                (1) Validação da lista de usuários inicial: `firstUserList`
+                (2) Criação de um novo usuário: `createRequest`
                 (3) Re-validação da lista, que deve conter o 
-                    novo registro
+                    novo registro: `secondUserList`
             */
             firstUserList = await chai
                 .request(server)
@@ -39,47 +72,43 @@ describe('Rota /api/users', () => {
             createRequest = await chai
                 .request(server)
                 .post('/api/users')
-                .send({
-                    username: 'jane',
-                    password: 'senha123'
-                });          
+                .send(newUser);          
             secondUserList = await chai
                 .request(server)
                 .get('/api/users')
                 .then(({body}) => body);
         });
 
-        after(async () => {
-            User.create.restore();
-            User.findAll.restore();
-        })
-
-        it('a lista inicial de pessoas usuárias deve conter 2 registros', () => {
+        it('firstUserList: A primeira requisição GET para a rota deve retornar 2 registros', () => {
             expect(firstUserList).to.have.length(2);
         });
 
-        it('retorna o código de status 201 após a inserção', () => {
+        it('createRequest: A requisição POST para a rota retorna o código de status 201', () => {
             expect(createRequest).to.have.status(201);
         });
 
-        it('retorna um objeto no corpo da resposta', () => {
+        it('createRequest: A requisição deve retornar um objeto no corpo da resposta', () => {
             expect(createRequest.body).to.be.a('object');
         });
 
-        it('o objeto possui a propriedade "message"', () => {
+        it('createRequest: O objeto possui a propriedade "message"', () => {
             expect(createRequest.body)
               .to.have.property('message');
         });
 
-        it('a propriedade "message" possui o texto "Novo usuário criado com sucesso"', 
+        it('createRequest: A propriedade "message" possui o texto "Novo usuário criado com sucesso"', 
           () => {
             expect(createRequest.body.message)
               .to.be.equal('Novo usuário criado com sucesso');
           }
         );
 
-        it('a lista de pessoas usuárias após a inserção deve conter 3 registros', () => {
+        it('secondUserList: A segunda requisição GET para rota deve retornar, por tanto, 3 registros', () => {
             expect(secondUserList).to.have.length(3);
         });
+
+        it('secondUserList: O registro criado deve corresponder ao enviado na requisição POST', () => {
+            expect(secondUserList[2]).to.contain(newUser);
+        })
     });
 });
