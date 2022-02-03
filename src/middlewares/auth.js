66e2aaa696e913d5a665d02e2f7d58834dfa2624
Roz <roz@rjmunhoz.me>
2022-02-03 15:44:22 -0300

    add eslint dependencies and config

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+
 const { JWT_KEY } = process.env;
+
+if (!JWT_KEY) {
+  const error = Error;
+  error.message = 'JWT_KEY não foi definido no .env';
+  throw error;
+}
 
 const { User } = require('../models');
 
 module.exports = async (req, res, next) => {
   try {
-    if (!JWT_KEY) {
-      console.error("JWT_KEY não foi definido no .env");
-      throw Error;
-    }
     const { authorization } = req.headers;
 
     const { username } = jwt.verify(authorization, JWT_KEY);
@@ -17,15 +20,13 @@ module.exports = async (req, res, next) => {
     const user = await User.findOne({ where: { username } });
 
     if (!authorization || !username || !user) throw Error;
-    
+
     next();
   } catch (err) {
     console.error(err);
-    res
-      .status(500)
-      .json({ 
-        message: 'Erro ao acessar o endpoint', 
-        error: 'É necessário um token válido para acessar esse endpoint' 
-      });
+    res.status(500).json({
+      message: 'Erro ao acessar o endpoint',
+      error: 'É necessário um token válido para acessar esse endpoint',
+    });
   }
-}
+};
