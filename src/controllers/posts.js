793cef5a909989ce3499f97aa05fdce1c16dbab5
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2022-08-29 06:48:40 -0300

    refactor: Ajusta lógica de posts com conteúdo

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,6 +1,6 @@
 const { PostService } = require('../services');
 
 module.exports = async (req, res) => {
-  const posts = await PostService.getPosts(req.user.id);
+  const posts = await PostService.getPosts(req);
   res.status(200).json({ mockPosts: posts });
 };
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,9 +1,17 @@
 const { Post } = require('../models');
 
-const getPosts = (userId) => Post.findAll({ 
-  where: { userId },
-  attributes: { exclude: 'id' },
-});
+const getPosts = async (request) => {
+  if (request.user) {
+    return Post.findAll({ 
+      where: { userId: request.user.id },
+      attributes: { exclude: 'id' },
+    });
+  }
+
+  return Post.findAll({
+    attributes: { exclude: 'id' },
+  });
+};
 
 module.exports = {
   getPosts,
