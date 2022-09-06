c8c19ccc0b80ae5914a05a3bd2b1b88d987acbe1
nato-re <alvares.renato@hotmail.com>
2022-09-06 19:34:24 -0300

    refactor to src

---
+++ src/migrations/20211207140145-posts.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Posts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable("Posts");
+  }
+};
---
+++ src/services/userService.js
@@ -0,0 +1,13 @@
+const { User } = require('../models');
+
+const createUser = (username, password) => User.create(username, password);
+
+const getUsers = () => User.findAll();
+
+const getByUsername = (username) => User.findOne({ where: { username } });
+
+module.exports = {
+  createUser,
+  getUsers,
+  getByUsername,
+};
