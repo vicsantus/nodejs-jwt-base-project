45b9d3710e8a5de8ef06289880fe78abce8c1ede
nato-re <alvares.renato@hotmail.com>
2022-09-07 20:13:54 -0300

    fix createUserService

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 const { User } = require('../models');
 
-const createUser = (username, password) => User.create(username, password);
+const createUser = ({ username, password }) => User.create({ username, password });
 
 const getUsers = () => User.findAll();
 
